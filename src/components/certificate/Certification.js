c29ab2a077ad6bbd551441c9fead99db73a33d28
Hugobertoncelo <hugobertoncelo@gmail.com>
2025-10-04 20:07:12 -0300

    new pages

---
+++ src/components/certificate/Certification.js
@@ -0,0 +1,83 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Zoom from "react-reveal/Zoom";
+import { Fade } from "react-reveal";
+import senai from "../../images/senai.jpg";
+import { Link } from "react-router-dom";
+import { HiArrowRight } from "react-icons/hi";
+
+export default function Certification() {
+  return (
+    <div>
+      <Container fluid className="certificate-section" id="about">
+        <Container>
+          <Row>
+            <Col
+              md={12}
+              className="certificate-description d-flex justify-content-start"
+            >
+              <Zoom left cascade>
+                <h1 className="aboutme-heading">Certificados</h1>
+              </Zoom>
+            </Col>
+            <Col ms={12} className="mt-3">
+              <Row className="g-5">
+                <Col md={4}>
+                  <Fade bottom duration={2000} distance="20px">
+                    <div className="cert-card">
+                      <div className="content">
+                        <a
+                          href="https://github.com/Hugobertoncelo/Certificados/blob/main/certificado.senai.pdf"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                        >
+                          <div className="content-overlay"></div>
+                          <div
+                            className="cert-header"
+                            style={{ backgroundColor: "white" }}
+                          >
+                            {<img className="logo_img" src={senai} alt={senai} />}
+                          </div>
+                          <div className="content-details fadeIn-top">
+                            <h3
+                              className="content-title"
+                              style={{ color: "black" }}
+                            >
+                              Certificado
+                            </h3>
+                          </div>
+                        </a>
+                      </div>
+                      <div className="cert-body">
+                        <h2
+                          className="cert-body-title"
+                          style={{ fontWeight: 700, color: "#fbd9ad" }}
+                        >
+                          Problem Solving Basic : Programming Fundamentals
+                        </h2>
+                        <h3
+                          className="cert-body-subtitle"
+                          style={{ color: "#eb90ff", marginBottom: "0px" }}
+                        >
+                          - HackerRank
+                        </h3>
+                      </div>
+                    </div>
+                  </Fade>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <div className="blog--viewAll">
+            <Link to="/certificatepage">
+              <button className="btn btn-primary">
+                Ver tudo
+                <HiArrowRight className="viewArr" />
+              </button>
+            </Link>
+          </div>
+        </Container>
+      </Container>
+    </div>
+  );
+}
---
+++ src/pages/project_page/ProjectPage.js
@@ -0,0 +1,105 @@
+import React from "react";
+import Particle from "../../Particle";
+import { Container, Row, Col } from "react-bootstrap";
+import Zoom from "react-reveal/Zoom";
+import { Fade } from "react-reveal";
+import nyc from "../../images/nyc.png";
+import { FaCode } from "react-icons/fa";
+export default function ProjectPage() {
+  return (
+    <section className="home-section">
+      <Container fluid id="home">
+        <Particle />
+        <Container className="home-content">
+          <div
+            className="d-flex justify-content-center"
+            width="100%"
+            style={{ backgroundColor: "#fbd9ad" }}
+          >
+            <Zoom left cascade>
+              <h1 style={{ color: "rgb(134 61 176" }}>PROJETOS</h1>
+            </Zoom>
+          </div>
+          <div>
+            <div>
+              <Container fluid className="certificate-section" id="about">
+                <Container>
+                  <Row>
+                    <Col md={12} className="mt-5">
+                      <Row className="g-5">
+                        <Col md={3} className="col-sm-12 col-md-4">
+                          <Fade bottom>
+                            <div
+                              key={1}
+                              className="singleProject"
+                              style={{
+                                backgroundColor: "rgb(142 70 186 / 31%)",
+                                border: "1px solid",
+                              }}
+                            >
+                              <div className="projectContent">
+                                <h5 id={"first"} style={{ color: "#fbd9ad" }}>
+                                  New Year CountDown
+                                </h5>
+                                <img src={nyc} alt={nyc} />
+                                <div className="project--showcaseBtn">
+                                  <a
+                                    href={
+                                      "https://github.com/MD-MAFUJUL-HASAN/Web_Programming/tree/main/New-Year-Countdown"
+                                    }
+                                    target="_blank"
+                                    rel="noreferrer"
+                                    className={"iconBtn"}
+                                    aria-labelledby={`code`}
+                                  >
+                                    <FaCode
+                                      id={`code`}
+                                      className={"icon"}
+                                      aria-label="Code"
+                                    />
+                                  </a>
+                                </div>
+                              </div>
+                              <h6>
+                                <p
+                                  className="project--desc"
+                                  style={{
+                                    background: "#fbd9ad",
+                                    color: "#b061df",
+                                    fontWeight: 600,
+                                  }}
+                                >
+                                  The New Year Countdown Project is more than
+                                  just a celebration; it's a testament to the
+                                  human spirit and our shared aspirations for a
+                                  brighter future. So, mark your calendars and
+                                  get ready to be part of this global
+                                  extravaganza as we count down to a new year
+                                  filled with endless possibilities.
+                                </p>
+                              </h6>
+                              <div
+                                className="project--lang"
+                                style={{
+                                  background: "#fbd9ad",
+                                  color: "#b061df",
+                                  fontWeight: 600,
+                                }}
+                              >
+                                HTML, CSS, JavaScript
+                              </div>
+                            </div>
+                          </Fade>
+                        </Col>
+                      </Row>
+                    </Col>
+                  </Row>
+                </Container>
+              </Container>
+            </div>
+          </div>
+        </Container>
+      </Container>
+    </section>
+  );
+}
