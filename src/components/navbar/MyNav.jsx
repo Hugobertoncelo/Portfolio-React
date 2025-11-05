fc7b3da73caac83407a5113099ce3b9d944a9b5d
Hugobertoncelo <hugobertoncelo@gmail.com>
2025-11-05 14:57:41 -0300

    New style for the form submit button.

--- src/components/navbar/MyNav.jsx
+++ src/components/navbar/MyNav.jsx
@@ -27,102 +27,139 @@ export default function MyNav() {
   window.addEventListener("scroll", scrollHandler);
 
   return (
-    <Navbar
-      expanded={expand}
-      fixed="top"
-      expand="md"
-      className={navColour ? "sticky" : "navbar"}
-    >
-      <Container>
-        <Navbar.Brand href="/">
-          <div className="d-flex flex-row justify-content-between">
-            <img src="./profile.png" className="img-fluid logo" alt="brand" />
-            <h5
-              id="navhead"
-              style={{
-                marginLeft: "21px",
-                paddingTop: "6px",
-                color: "#fbd9ad",
-              }}
-            >
-              HUGO BERTONCELO
-            </h5>
-          </div>
-        </Navbar.Brand>
-        <Navbar.Toggle
-          aria-controls="responsive-navbar-nav"
-          onClick={() => {
-            updateExpanded(expand ? false : "expanded");
-          }}
-        >
-          <span></span>
-          <span></span>
-          <span></span>
-        </Navbar.Toggle>
-        <Navbar.Collapse id="responsive-navbar-nav" className="navbarmain">
-          <Nav className="ml-auto" defaultActiveKey="#home">
-            <Nav.Item>
-              <Nav.Link as={Link} to="/" onClick={() => updateExpanded(false)}>
-                <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
-              </Nav.Link>
-            </Nav.Item>
+    <>
+      <style>
+        {`
+          .underline-name {
+            position: relative;
+            display: inline-block;
+            color: #fbd9ad;
+            font-weight: 600;
+            letter-spacing: 1px;
+          }
 
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/projectspage"
-                onClick={() => updateExpanded(false)}
-              >
-                <AiOutlineFundProjectionScreen
-                  style={{ marginBottom: "2px" }}
-                />{" "}
-                Projetos
-              </Nav.Link>
-            </Nav.Item>
+          .underline-name::after {
+            content: "";
+            position: absolute;
+            left: 0;
+            bottom: -3px;
+            width: 100%;
+            height: 2px;
+            background: linear-gradient(90deg, #fbd9ad, #ffe8b3, #fbd9ad);
+            background-size: 200%;
+            animation: underlineMove 3s ease-in-out infinite;
+            border-radius: 2px;
+          }
 
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/resume"
-                onClick={() => updateExpanded(false)}
-              >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resumo
-              </Nav.Link>
-            </Nav.Item>
+          @keyframes underlineMove {
+            0% { background-position: 0%; }
+            50% { background-position: 100%; }
+            100% { background-position: 0%; }
+          }
+        `}
+      </style>
 
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/certificatepage"
-                onClick={() => updateExpanded(false)}
+      <Navbar
+        expanded={expand}
+        fixed="top"
+        expand="md"
+        className={navColour ? "sticky" : "navbar"}
+      >
+        <Container>
+          <Navbar.Brand href="/">
+            <div className="d-flex flex-row justify-content-between">
+              <img src="./profile.png" className="img-fluid logo" alt="brand" />
+              <h5
+                id="navhead"
+                className="underline-name"
+                style={{
+                  marginLeft: "21px",
+                  paddingTop: "6px",
+                }}
               >
-                <ImBlog style={{ marginBottom: "2px" }} /> Certificados
-              </Nav.Link>
-            </Nav.Item>
+                HUGO BERTONCELO
+              </h5>
+            </div>
+          </Navbar.Brand>
+          <Navbar.Toggle
+            aria-controls="responsive-navbar-nav"
+            onClick={() => {
+              updateExpanded(expand ? false : "expanded");
+            }}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </Navbar.Toggle>
+          <Navbar.Collapse id="responsive-navbar-nav" className="navbarmain">
+            <Nav className="ml-auto" defaultActiveKey="#home">
+              <Nav.Item>
+                <Nav.Link
+                  as={Link}
+                  to="/"
+                  onClick={() => updateExpanded(false)}
+                >
+                  <AiOutlineHome style={{ marginBottom: "2px" }} /> Home
+                </Nav.Link>
+              </Nav.Item>
 
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/contact"
-                onClick={() => updateExpanded(false)}
-              >
-                <AiOutlineMail style={{ marginBottom: "2px" }} /> Contatos
-              </Nav.Link>
-            </Nav.Item>
+              <Nav.Item>
+                <Nav.Link
+                  as={Link}
+                  to="/projectspage"
+                  onClick={() => updateExpanded(false)}
+                >
+                  <AiOutlineFundProjectionScreen
+                    style={{ marginBottom: "2px" }}
+                  />{" "}
+                  Projetos
+                </Nav.Link>
+              </Nav.Item>
 
-            <Nav.Item className="fork-btn">
-              <Button
-                href="https://github.com/Hugobertoncelo/Portfolio-React"
-                target="_blank"
-                className="fork-btn-inner"
-              >
-                <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
-                <AiFillStar style={{ fontSize: "1.1em" }} />
-              </Button>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+              <Nav.Item>
+                <Nav.Link
+                  as={Link}
+                  to="/resume"
+                  onClick={() => updateExpanded(false)}
+                >
+                  <CgFileDocument style={{ marginBottom: "2px" }} /> Resumo
+                </Nav.Link>
+              </Nav.Item>
+
+              <Nav.Item>
+                <Nav.Link
+                  as={Link}
+                  to="/certificatepage"
+                  onClick={() => updateExpanded(false)}
+                >
+                  <ImBlog style={{ marginBottom: "2px" }} /> Certificados
+                </Nav.Link>
+              </Nav.Item>
+
+              <Nav.Item>
+                <Nav.Link
+                  as={Link}
+                  to="/contact"
+                  onClick={() => updateExpanded(false)}
+                >
+                  <AiOutlineMail style={{ marginBottom: "2px" }} /> Contatos
+                </Nav.Link>
+              </Nav.Item>
+
+              <Nav.Item className="fork-btn">
+                <Button
+                  href="https://github.com/Hugobertoncelo/Portfolio-React"
+                  target="_blank"
+                  className="fork-btn-inner"
+                >
+                  <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
+                  <AiFillStar style={{ fontSize: "1.1em" }} />
+                </Button>
+              </Nav.Item>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
   );
 }
