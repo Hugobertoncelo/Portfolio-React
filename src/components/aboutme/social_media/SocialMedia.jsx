a58ea2b2c538e462bbf4e78b25aa007a43a239ba
Hugobertoncelo <hugobertoncelo@gmail.com>
2025-11-10 10:12:58 -0300

    New link to old portfolio

--- src/components/aboutme/social_media/SocialMedia.jsx
+++ src/components/aboutme/social_media/SocialMedia.jsx
@@ -5,8 +5,8 @@ import {
   AiFillGithub,
   AiFillLinkedin,
 } from "react-icons/ai";
-import { Container, Row } from "react-bootstrap";
-import { Col } from "react-bootstrap";
+import { FaGlobe } from "react-icons/fa";
+import { Container, Row, Col } from "react-bootstrap";
 import Zoom from "react-reveal/Zoom";
 
 export default function SocialMedia() {
@@ -22,46 +22,67 @@ export default function SocialMedia() {
               href="https://www.instagram.com/bertoncelo.hugo"
               target="_blank"
               rel="noreferrer"
-              className="icon-colour  home-social-icons"
+              className="icon-colour home-social-icons"
+              title="Instagram"
             >
               <Zoom cascade>
                 <AiOutlineInstagram />
               </Zoom>
             </a>
           </Col>
+
           <Col md={3}>
             <a
               href="https://wa.me/5528999453033"
               target="_blank"
               rel="noreferrer"
-              className="icon-colour  home-social-icons"
+              className="icon-colour home-social-icons"
+              title="WhatsApp"
             >
               <Zoom cascade>
                 <AiOutlineWhatsApp />
               </Zoom>
             </a>
           </Col>
+
           <Col md={3}>
             <a
               href="https://github.com/Hugobertoncelo"
               target="_blank"
               rel="noreferrer"
-              className="icon-colour  home-social-icons"
+              className="icon-colour home-social-icons"
+              title="GitHub"
             >
               <Zoom cascade>
                 <AiFillGithub />
               </Zoom>
             </a>
           </Col>
+
           <Col md={3}>
             <a
               href="https://www.linkedin.com/in/hugobertoncelo"
               target="_blank"
               rel="noreferrer"
-              className="icon-colour  home-social-icons"
+              className="icon-colour home-social-icons"
+              title="LinkedIn"
             >
               <Zoom cascade>
                 <AiFillLinkedin />
+              </Zoom>
+            </a>
+          </Col>
+
+          <Col md={3}>
+            <a
+              href="https://hugobertoncelo.github.io/Portfolio/"
+              target="_blank"
+              rel="noreferrer"
+              className="icon-colour home-social-icons"
+              title="Portfólio antigo"
+            >
+              <Zoom cascade>
+                <FaGlobe />
               </Zoom>
             </a>
           </Col>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { AiFillGithub, AiFillInstagram } from "react-icons/ai";
-import { FaLinkedinIn, FaWhatsapp, FaGoogle } from "react-icons/fa";
+import { FaLinkedinIn, FaWhatsapp, FaGoogle, FaGlobe } from "react-icons/fa";
 
 function Footer() {
   let date = new Date();
   let year = date.getFullYear();
+
   return (
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
           <h3>Projetado e desenvolvido por Hugo Bertoncelo</h3>
         </Col>
+
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} Hugo Bertoncelo</h3>
         </Col>
+
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
             <li className="social-icons">
@@ -23,36 +26,43 @@ function Footer() {
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
+                title="GitHub"
               >
                 <AiFillGithub />
               </a>
             </li>
+
             <li className="social-icons">
               <a
                 href="https://www.linkedin.com/in/hugobertoncelo"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
+                title="LinkedIn"
               >
                 <FaLinkedinIn />
               </a>
             </li>
+
             <li className="social-icons">
               <a
                 href="https://www.instagram.com/bertoncelo.hugo"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
+                title="Instagram"
               >
                 <AiFillInstagram />
               </a>
             </li>
+
             <li className="social-icons">
               <a
                 href="https://wa.me/5528999453033"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
+                title="WhatsApp"
               >
                 <FaWhatsapp />
               </a>
@@ -64,8 +74,21 @@ function Footer() {
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
+                title="E-mail"
               >
                 <FaGoogle />
+              </a>
+            </li>
+
+            <li className="social-icons">
+              <a
+                href="https://hugobertoncelo.github.io/Portfolio/"
+                style={{ color: "white" }}
+                target="_blank"
+                rel="noopener noreferrer"
+                title="Portfólio antigo"
+              >
+                <FaGlobe />
               </a>
             </li>
           </ul>
